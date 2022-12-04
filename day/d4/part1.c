f7d1e04efa57e4b94283bcf807299aaad5a31a11
Aitor <aitor.izuzquiza@gmail.com>
2022-12-04 11:11:07 +0100

    refactor

--- day/d4/part1.c
+++ day/d4/part1.c
@@ -9,16 +9,13 @@ int contains(struct Interval *i1,struct Interval *i2){
 }
 
 int main() {
-    // sum = 0, open file, read line,
     int sum = 0;
     char line[12];
     struct Interval i1, i2;
     FILE *f = fopen("input.txt", "r");
     while((fgets(line, 121, f)) != NULL) {
         sscanf(line, "%d-%d,%d-%d",&i1.begin, &i1.end, &i2.begin, &i2.end);
-        if(contains(&i1, &i2)) {
-            sum++;
-        }
+        if(contains(&i1, &i2)) sum++;
     }
     fclose(f);
     printf("Total: %d\n", sum);
--- day/d4/part2.c
+++ day/d4/part2.c
@@ -20,16 +20,13 @@ int overlaps(struct Interval *i1,struct Interval *i2){
 }
 
 int main() {
-    // sum = 0, open file, read line,
     int sum = 0;
     char line[12];
     struct Interval i1, i2;
     FILE *f = fopen("input.txt", "r");
     while((fgets(line, 121, f)) != NULL) {
         sscanf(line, "%d-%d,%d-%d",&i1.begin, &i1.end, &i2.begin, &i2.end);
-        if(overlaps(&i1, &i2)) {
-            sum++;
-        }
+        if(overlaps(&i1, &i2)) sum++;
     }
     fclose(f);
     printf("Total: %d\n", sum);
