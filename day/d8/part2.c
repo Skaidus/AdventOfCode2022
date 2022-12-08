a3c880dc9932eb36598546a7f1fd0e002aee534f
Skaidus <aitor.izuzquiza@gmail.com>
2022-12-08 22:10:21 +0100

    Part 2

--- day/d8/part2.c
+++ day/d8/part2.c
@@ -2,68 +2,63 @@
 
 struct Tree {
     char height;
-    int seen;
+    int score;
 };
 
 int const grid_size = 99;
 
 int main() {
-    int sum = 0, n_line = 0;
+    int max = 0, n_line = 0;
     char line[grid_size];
     struct Tree grid[grid_size][grid_size];
     FILE *f = fopen("./input.txt", "r");
-    char max_height;
+
     while((fgets(line, grid_size+3, f)) != NULL) {
         for(int i = 0; i < grid_size; i++) {
             grid[n_line][i].height = line[i];
-            grid[n_line][i].seen = 0;
+            grid[n_line][i].score = 1;
         }
-        max_height = '.';
-        for(int i = 0; i < grid_size; i++) {
-            if(grid[n_line][i].height > max_height) {
-                max_height = grid[n_line][i].height;
-                if(grid[n_line][i].seen == 0) {
-                    grid[n_line][i].seen = 1;
-                    sum ++;
-                }
+        for(int i = 1; i < grid_size-1; i++) {
+            int local_score = 0;
+            for(int j = i-1; j >= 0; j--){
+                local_score++;
+                if(grid[n_line][j].height >= grid[n_line][i].height) break;
             }
+            grid[n_line][i].score *= local_score;
         }
-        max_height = '.';
-        for(int i = grid_size-1; i >= 0; i--) {
-            if(grid[n_line][i].height > max_height) {
-                max_height = grid[n_line][i].height;
-                if(grid[n_line][i].seen == 0) {
-                    grid[n_line][i].seen = 1;
-                    sum ++;
-                }
+        for(int i = grid_size - 2; i > 0; i--) {
+            int local_score = 0;
+            for(int j = i+1; j < grid_size; j++){
+                local_score++;
+                if(grid[n_line][j].height >= grid[n_line][i].height) break;
             }
+            grid[n_line][i].score *= local_score;
         }
+
         n_line++;
     }
-    for(int j = 0; j < grid_size; j++) {
-        max_height = '.';
-        for(int i  = 0; i < grid_size; i++) {
-            if(grid[i][j].height > max_height) {
-                max_height = grid[i][j].height;
-                if(grid[i][j].seen == 0) {
-                    grid[i][j].seen = 1;
-                    sum ++;
-                }
+
+    for(int k = 1; k < grid_size-1; k++) {
+        for(int i = 1; i < grid_size-1; i++) {
+            int local_score = 0;
+            for(int j = i-1; j >= 0; j--){
+                local_score++;
+                if(grid[j][k].height >= grid[i][k].height) break;
             }
+            grid[i][k].score *= local_score;
         }
-        max_height = '.';
-        for(int i = grid_size-1; i >= 0; i--) {
-            if(grid[i][j].height > max_height) {
-                max_height = grid[i][j].height;
-                if(grid[i][j].seen == 0) {
-                    grid[i][j].seen = 1;
-                    sum ++;
-                }
+        for(int i = grid_size - 2; i > 0; i--) {
+            int local_score = 0;
+            for(int j = i+1; j < grid_size; j++){
+                local_score++;
+                if(grid[j][k].height >= grid[i][k].height) break;
             }
+            grid[i][k].score *= local_score;
+            if(grid[i][k].score>max) max = grid[i][k].score;
         }
     }
 
     fclose(f);
-    printf("Total: %d\n", sum);
+    printf("Max visibility: %d\n", max);
     return 0;
 }
