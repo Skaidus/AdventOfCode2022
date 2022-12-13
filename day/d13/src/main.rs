4b5c64e8602193a857f13c762c1156b260fb7789
Skaidus <aitor.izuzquiza@gmail.com>
2022-12-13 22:26:19 +0100

    added code

--- day/d13/src/main.rs
+++ day/d13/src/main.rs
@@ -3,6 +3,27 @@ enum Packet {
     Integer(u32)
 }
 
+
+
+impl Packet {
+    fn le(&self, other: &Self) -> bool {
+        return match (self, other){
+            (Self::Integer(a), Self::Integer(b))=> a<=b,
+            (Self::List(a), Self::List(b))=>{
+                for i in 0..a.len().min(b.len()) {
+                    if !a[i].le(&b[i]){
+                        return false
+                    }
+                }
+                a.len() <= b.len()
+            },
+            (Self::List(_), Self::Integer(b))=>
+                self.le(&Packet::List(vec![Packet::Integer(*b)])),
+            (Self::Integer(_), Self::List(_))=> self.le(other)
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
