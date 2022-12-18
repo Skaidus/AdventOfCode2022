5737a021adf3b888b30b12f15251af1e5bd6def4
Skaidus <aitor.izuzquiza@gmail.com>
2022-12-18 23:25:02 +0100

    Almost finished

--- day/d14/src/main.rs
+++ day/d14/src/main.rs
@@ -17,18 +17,19 @@ fn get_units(mut grid : Vec<Vec<bool>>) -> u32 {
     let rows : i32 = grid.len() as i32; 
     let cols : i32 = grid[0].len() as i32;
     let directions = [(1,0), (1,-1), (1,1)];
+    // REFORMAR: AHORA MISMO SE CHECKEA SI BLOCKED OUT OF LIMITS
     loop {
         units+=1;
         while (0<= pos.0) && (pos.0 < rows) && (0<= pos.1) && (pos.1 < cols) {
             for dir in directions{
                 let candidate = (pos.0 + dir.0, pos.1 + dir.1);
-                if grid[candidate.0 as usize][candidate.1 as usize] {
+                println!("({} {})", candidate.0, candidate.1);
+                if (0<= candidate.0) && (candidate.0 < rows) && (0<= candidate.1) && (candidate.1 < cols) && grid[candidate.0 as usize][candidate.1 as usize] {
                     pos = candidate;
                     break
                 }
             }
             grid[pos.0 as usize][pos.1 as usize] = true;
-            break
         }
         return units
     }
@@ -38,11 +39,10 @@ fn main() {
     
     if let Ok(lines) = read_lines("input.txt") {
         // Consumes the iterator, returns an (Optional) String
-        let (mut sum, mut value, mut line_count) = (0,1,0);
         let (mut min_i, mut max_i, mut min_j, mut max_j) = (MAX, MIN, MAX, MIN);
         for line in lines {
             if let Ok(ip) = line {  
-                ip.split(" -> ").for_each(move |x|{
+                for x in ip.split(" -> "){
                     let mut y = x.split(',')
                     .map(|x| x.parse::<i32>().expect("parse error"));
                     let i = y.next().unwrap();
@@ -51,17 +51,48 @@ fn main() {
                     let j = y.next().unwrap();
                     min_j = min_j.min(j);
                     max_j = max_j.max(j);
-                });
+                }
             }
         }
         
+        let mut grid = vec![vec![false; (max_j + 1 - min_j) as usize]; (max_i + 1 - min_i) as usize];
+        if let Ok(lines) = read_lines("input.txt") {
+        
+        for line in lines {
+            if let Ok(ip) = line {
+                let mut corners = ip.split(" -> ")
+                .map(|x| x.split(',').map(|x| x.parse::<i32>().unwrap())).map(|mut x| ((x.next().unwrap()-min_i) as usize, (x.next().unwrap()-min_j) as usize)).into_iter();
+                let mut begin  = corners.next().unwrap();
+                for corner in corners {
+                    if corner.0 == begin.0 {
+                        for x1 in begin.1..corner.1 {
+                            grid[corner.0][x1] = true;
+                        }
+                    } else {
+                        for x0 in begin.0..corner.0 {
+                            grid[x0][corner.1] = true;
+                        }
+                    }
+                    begin = corner;
+                }
+            }
+        }
+        println!("Blocks: {}", get_units(grid));
+    }
 
         // let next_cycle = ((line_count-20)/40 + 1)*40 + 20;
         // for c in (next_cycle..=220).step_by(40){
         //     sum += c * value;
         // }
-        println!("total size: {}", sum);
+        
 
     }
+
+ 
+        
+
+        // let next_cycle = ((line_count-20)/40 + 1)*40 + 20;
+        // for c in (next_cycle..=220).step_by(40){
+
         
 }
