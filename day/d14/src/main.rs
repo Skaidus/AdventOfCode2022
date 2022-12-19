384f98f8eff3f3bbcd2f1c0c3713666301e73953
Aitor <aitor.izuzquiza@gmail.com>
2022-12-19 21:09:17 +0100

    trying to fix

--- day/d14/src/main.rs
+++ day/d14/src/main.rs
@@ -1,4 +1,4 @@
-use std::i32::{MAX,MIN};
+use std::i32::MIN;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
@@ -11,12 +11,13 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn get_units(mut grid : Vec<Vec<bool>>) -> u32 {
+fn get_units(mut grid : Vec<Vec<bool>>, column_offset : i32) -> u32 {
     let mut units : u32 = 0;
-    let mut pos = (0,0);
+    let mut pos = (0,500-column_offset);
     let rows : i32 = grid.len() as i32; 
     let cols : i32 = grid[0].len() as i32;
     let directions = [(1,0), (1,-1), (1,1)];
+    let mut out_limits = false;
     // REFORMAR: AHORA MISMO SE CHECKEA SI BLOCKED OUT OF LIMITS
     loop {
         units+=1;
@@ -24,12 +25,14 @@ fn get_units(mut grid : Vec<Vec<bool>>) -> u32 {
             for dir in directions{
                 let candidate = (pos.0 + dir.0, pos.1 + dir.1);
                 println!("({} {})", candidate.0, candidate.1);
-                if (0<= candidate.0) && (candidate.0 < rows) && (0<= candidate.1) && (candidate.1 < cols) && grid[candidate.0 as usize][candidate.1 as usize] {
+                out_limits = !((0<= candidate.0) && (candidate.0 < rows) && (0<= candidate.1) && (candidate.1 < cols));
+                if out_limits || grid[candidate.0 as usize][candidate.1 as usize] {
                     pos = candidate;
                     break
                 }
             }
-            grid[pos.0 as usize][pos.1 as usize] = true;
+            if !out_limits {grid[pos.0 as usize][pos.1 as usize] = true;}
+
         }
         return units
     }
@@ -39,14 +42,13 @@ fn main() {
     
     if let Ok(lines) = read_lines("input.txt") {
         // Consumes the iterator, returns an (Optional) String
-        let (mut min_i, mut max_i, mut min_j, mut max_j) = (MAX, MIN, MAX, MIN);
+        let (mut max_i, mut min_j, mut max_j) = (MIN, 500, 500);
         for line in lines {
             if let Ok(ip) = line {  
                 for x in ip.split(" -> "){
                     let mut y = x.split(',')
                     .map(|x| x.parse::<i32>().expect("parse error"));
                     let i = y.next().unwrap();
-                    min_i = min_i.min(i);
                     max_i = max_i.max(i);
                     let j = y.next().unwrap();
                     min_j = min_j.min(j);
@@ -55,13 +57,13 @@ fn main() {
             }
         }
         
-        let mut grid = vec![vec![false; (max_j + 1 - min_j) as usize]; (max_i + 1 - min_i) as usize];
+        let mut grid = vec![vec![false; (max_j + 1 - min_j) as usize]; (max_i + 1) as usize];
         if let Ok(lines) = read_lines("input.txt") {
         
         for line in lines {
             if let Ok(ip) = line {
                 let mut corners = ip.split(" -> ")
-                .map(|x| x.split(',').map(|x| x.parse::<i32>().unwrap())).map(|mut x| ((x.next().unwrap()-min_i) as usize, (x.next().unwrap()-min_j) as usize)).into_iter();
+                .map(|x| x.split(',').map(|x| x.parse::<i32>().unwrap())).map(|mut x| (x.next().unwrap() as usize, (x.next().unwrap()-min_j) as usize)).into_iter();
                 let mut begin  = corners.next().unwrap();
                 for corner in corners {
                     if corner.0 == begin.0 {
@@ -77,7 +79,8 @@ fn main() {
                 }
             }
         }
-        println!("Blocks: {}", get_units(grid));
+        
+        println!("Blocks: {}", get_units(grid, min_j));
     }
 
         // let next_cycle = ((line_count-20)/40 + 1)*40 + 20;
