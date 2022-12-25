bb11bcc1e6f1d7810d1ef249e8291401baa58f2c
Aitor <aitor.izuzquiza@gmail.com>
2022-12-25 21:14:40 +0100

    Added parser

--- day/d13/src/main.rs
+++ day/d13/src/main.rs
@@ -1,7 +1,15 @@
+
+use std::str::Split;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
 enum Packet {
     List(Vec<Packet>),
     Integer(u32)
 }
+
 
 
 
@@ -24,6 +32,43 @@ impl Packet {
     }
 }
 
+
+fn get_packet(tokens: &mut Split<&str>) -> Packet{
+    let mut packet : Vec<Packet> = vec![];
+    loop {
+        match tokens.next() {
+            Some(t) => {
+                match t {
+                    "]" => break,
+                    "[" => packet.push(get_packet(tokens)),
+                    "" => {},
+                    _ => packet.push(Packet::Integer(t.parse::<u32>().unwrap()))
+                }
+            },
+            None => break
+        }
+    }
+    return Packet::List(packet)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt").unwrap();
+    let mut reader = io::BufReader::new(file).lines().into_iter();
+    
+    let mut index = 1;
+    let mut sum = 0;
+    loop {
+        match reader.next() {
+            Some(Ok(ip)) => {
+                let ip = ip.replace("[", "[,").replace("]", ",]");
+                let packet1 = get_packet(&mut ip.split(","));
+                let packet2 = get_packet(&mut reader.next().unwrap().unwrap().replace("[", "[,").replace("]", ",]").split(","));
+                if packet1.le(&packet2) {sum+=index}
+                index+=1;
+                reader.next();
+            },
+            _ => break
+        }
+    }
+    
 }
